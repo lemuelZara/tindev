3244b311837003e295780830708e9806f324b1fe
lemuelZara <lemuel.czara@gmail.com>
2020-10-05 23:31:24 -0300

    create a shell script to add new user to mongodb container

--- backend/mongodb-init.sh
+++ backend/mongodb-init.sh
@@ -6,27 +6,18 @@
 #              e cria um novo usuário.
 set -Eeuo pipefail
 
-echo "$(tput setaf 5) [MongoDB] Inicializing script..."
-
-if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g' | xargs) | envsubst)
-fi
-
-MONGO_USER=$MONGO_USER
-MONGO_PASSWORD=$MONGO_PASSWORD
-MONGO_DB=$MONGO_DB
-
+echo "[MongoDB] Inicializing script..."
 # Checks whether the values ​​of MONGO_USER and MONGO_PASSWORD are not empty
-if [ -n "$MONGO_USER" -a -n "$MONGO_PASSWORD" ]
+
+if [ -n $MONGO_USER -a -n $MONGO_PASSWORD ]
 then
-  mongo --host localhost -u root -p root --authenticationDatabase admin<<EOF
-    db = db.getSiblingDB('$MONGO_DB')
-    use $MONGO_DB
-    db.createUser({
-      user: '$MONGO_USER',
-      pwd: '$MONGO_PASSWORD',
-      roles: [{ role: 'readWrite', db: '$MONGO_DB' }]
-    })
+  mongo --host localhost -u $MONGO_INITDB_USER -p $MONGO_INITDB_PASSWORD --authenticationDatabase admin <<EOF
+  use tindev
+  db.createUser({
+    user: '$MONGO_USER',
+    pwd: '$MONGO_PASSWORD',
+    roles: [{ role: 'readWrite', db: 'tindev' }]
+  })
 EOF
 fi
 

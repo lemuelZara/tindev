2e28ee6346059b5a15a1ec448823b3bd4ed33500
lemuelZara <lemuel.czara@gmail.com>
2020-10-05 23:33:50 -0300

    application server express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
+import './database/connection';
 
 const app = express();
 
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333);

756784f4964743630428cd9abba239278deafe27
lemuelZara <lemuel.czara@gmail.com>
2020-10-05 23:32:53 -0300

    application routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import DevController from './controllers/DevController';
+import DislikeController from './controllers/DislikeController';
+import LikeController from './controllers/LikeController';
+
 const routes = Router();
 
-routes.get('/', (_, res) => {
-  return res.status(200).json({
-    message: 'Ok!a',
-  });
-});
+routes.post('/devs', DevController.store);
+routes.post('/devs/:id/likes', LikeController.store);
+routes.post('/devs/:id/dislikes', DislikeController.store);
+
+routes.get('/devs', DevController.index);
 
 export default routes;

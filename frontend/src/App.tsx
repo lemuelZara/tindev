109d5f7714c44f7d7b9df8ced6544e4c83c09fbc
lemuelZara <lemuel.czara@gmail.com>
2020-10-07 01:42:05 -0300

    eslint and prettier configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const luke = {
+    jedi: true,
+    age: 28,
+    name: 'a',
+  };
+
+  // bad
+  const isJedi = luke.age;
+
   return (
     <div className="App">
-      <h1>Tindev</h1>
+      <h1> Tindev</h1>
     </div>
   );
-}
+};
 
 export default App;

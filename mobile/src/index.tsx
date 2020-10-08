08a0357188690daab3308e417295d057a7b6d288
lemuelZara <lemuel.czara@gmail.com>
2020-10-08 16:44:56 -0300

    default App component

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Login from './pages/Login';
+import Routes from './routes';
 
 const App = () => {
-  return <Login />;
+  return <Routes />;
 };
 
 export default App;

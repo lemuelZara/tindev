ac5f6c6029b906d03fad2eece6e047e0afe165de
lemuelZara <lemuel.czara@gmail.com>
2020-10-09 15:02:44 -0300

    main page

---
+++ mobile/src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Main: React.FC = () => {
+  return (
+    <View>
+      <Text>Main.tsx</Text>
+    </View>
+  );
+};
+
+export default Main;

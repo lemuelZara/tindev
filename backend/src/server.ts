b973e75e39624a1beb0141c580b89942690b5907
lemuelZara <lemuel.czara@gmail.com>
2020-10-11 01:59:46 -0300

    socket-io configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,41 @@
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import socketIO from 'socket.io';
 
 import routes from './routes';
 import './database/connection';
 
 const app = express();
 
+const httpServer = new http.Server(app);
+const io = socketIO(httpServer);
+
+interface IConnectedUsers {
+  [userId: string]: string;
+}
+
+const connectedUsers = {} as IConnectedUsers;
+
+io.on('connection', (socket) => {
+  const { user } = socket.handshake.query;
+
+  console.table({
+    userId: user,
+    socketId: socket.id,
+  });
+
+  connectedUsers[user] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+httpServer.listen(3333);

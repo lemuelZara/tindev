26fcd29edba1700bb97390ee1cca7092807eb150
lemuelZara <lemuel.czara@gmail.com>
2020-10-14 11:30:50 -0300

    using LogBox to ignore logs from socket.io

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 
 import Routes from './routes';
+
+LogBox.ignoreLogs(['Unrecognized WebSocket']);
 
 const App = () => {
   return <Routes />;
